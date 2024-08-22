bdbc7070a633bfd23d750ae9fea17c2271d306d0
bear81 <alongcameabear@gmail.com>
2024-08-22 14:15:22 +0100

    Update Models for Post

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+# Every model you create is a Python class that subclasses the parent class of # models.Model.
+
 
 # Create your models here.
 
@@ -14,6 +16,8 @@ class Post(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
+    excerpt = models.TextField(blank=True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    updated_on = models.DateTimeField(auto_now=True)
